189432e51fa22521c384f77f94f8f010ea4a5968
iamel-g <mgalvan@outlook.fr>
2022-09-24 12:54:37 +0200

    Variable bases + exercices

---
+++ variables.php
@@ -0,0 +1,18 @@
+<?php
+# Presentation
+$prenom = 'Melissa';
+$nom = 'Galvan';
+$age = 28;
+echo 'Je m\'appelle ' . $prenom . ' ' . $nom . ' et j\'ai ' . $age . ' ans.'; // Je m'appelle Melissa Galvan et j'ai 28 ans.
+
+# Moyenne
+$note1 = 10;
+$note2 = 15;
+$moyenne = ($note1 + $note2) / 2;
+echo $moyenne; // 12.5
+# NULL
+echo $note3 = NULL; // Rien
+
+# Exercice 
+echo 'Bonjour ' . $prenom . ' ' . $nom . ' vous avez eu ' . $moyenne . ' de moyenne.';
+// echo "\nBonjour $prenom $nom vous avez eu $moyenne de moyenne.\n";
